020ff98eb3bfa5ff61d675ab7495691694ce2ba3
Jiwoo <58379240+jiwoojung0206@users.noreply.github.com>
2020-04-13 23:50:33 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,13 +5,34 @@ using namespace std;
 
 int main() {
     
-    int num1{};
+    int num{};
     
-    cout << "Enter an integer:";
-    cin >> num1;
+    cout << "Enter an integer less than 100:";
+    cin >> num;
     
-    cout << num1 << " is" << ((num1 % 2 == 0) ? " even" : " odd");
+    while (num >= 100) {
+        cout << "Enter an integer less than 100:";
+        cin >> num;
+    }
+    
+    cout << "Thanks" << endl;
+    
+    bool done{false};
+    int number{};
+    
+    while (!done) {
+        cout << "Enter an integer between 1 and 10:";
+        cin >> number;
+        
+        if (number <= 1 || number >= 10)
+            cout << "Try again" << endl;
+            
+        else {cout << "Thanks";
+        done = true;
+        }
+    }
+    
     
     cout << endl;
     return 0;
-} 
+}
